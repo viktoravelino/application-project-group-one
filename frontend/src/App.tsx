ae179350e3e3992bb35561cb5337b558944d6d84
Viktor Avelino <viktor.avelino@gmail.com>
2022-02-12 11:14:50 -0500

    Change root route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router";
 import { LoginPage } from "./pages/LoginPage";
 import { RegisterPage } from "./pages/RegisterPage";
-import MainPage from "./pages/MainPage";
 
 // Main Application
 import { MainLayoutContainer } from "./pages/MainLayoutContainer";
@@ -11,11 +10,10 @@ function App() {
     <div className="app">
       {/* <h1>Testing Routes</h1> */}
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route path="/" element={<MainLayoutContainer />} />
+
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-
-        <Route path="/testing" element={<MainLayoutContainer />} />
       </Routes>
     </div>
   );
