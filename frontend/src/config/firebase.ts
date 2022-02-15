4a3614ce62c011d6e0b640917d688288760ac739
Viktor Avelino <viktor.avelino@gmail.com>
2022-02-15 12:05:28 -0500

    firebase config file

---
+++ frontend/src/config/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCDjaR21WuebLReVFrDkRZ7bhqADAwIysc",
+  authDomain: "application-project-group-1.firebaseapp.com",
+  projectId: "application-project-group-1",
+  storageBucket: "application-project-group-1.appspot.com",
+  messagingSenderId: "241466451563",
+  appId: "1:241466451563:web:fabe7e291c4bc1aa3fb32e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+export { auth };
