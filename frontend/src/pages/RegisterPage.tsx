aa7fda35e0962c092b13ac09511428e200519b5c
Viktor Avelino <viktor.avelino@gmail.com>
2022-02-22 09:28:35 -0500

    refactor: register using AuthContext

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -9,40 +9,30 @@ import Github from "../components/Github";
 //importing icons
 import { BiUser } from "react-icons/bi";
 import { AiOutlineUnlock } from "react-icons/ai";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { useAuth } from "../context/AuthContext";
 
 export function RegisterPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const { registerUserWithEmailAndPassword } = useAuth();
+
   const handleSubmit = async (e: any) => {
     e.preventDefault();
-    const auth = getAuth();
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-        // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
-      });
-  setEmail("");
-  setPassword("");
+    if (!email || !password) {
+      return;
+    }
+    registerUserWithEmailAndPassword(email, "test", password);
   };
   //email handler
-  const handleEmail = (e : any) => {
-    console.log(e.target.value);
+  const handleEmail = (e: any) => {
     setEmail(e.target.value);
-  }
+  };
 
-   //passowrd handler
-   const handlePwd = (e : any) => {
-    console.log(e.target.value)
+  //passowrd handler
+  const handlePwd = (e: any) => {
     setPassword(e.target.value);
-  }
+  };
 
   return (
     <div id="loginPage" className="bg-bgColor text-center">
@@ -50,9 +40,7 @@ export function RegisterPage() {
         id="formContainer"
         className="flex justify-center items-center h-5/6  w-screen"
       >
-        <form
-          className="mx-auto mb-0 space-y-4 bg-white  rounded-form w-80 h-4/5 rounded-2xl"
-        >
+        <form className="mx-auto mb-0 space-y-4 bg-white  rounded-form w-80 h-4/5 rounded-2xl">
           <div className="flex justify-center p-5">
             <Logo />
           </div>
@@ -69,7 +57,9 @@ export function RegisterPage() {
 
             <input
               className="w-4/5 p-0 pl-30 text-sm outline-0 border-b border-primaryColor text-center"
-              placeholder="email address" onChange={handleEmail} value={email} 
+              placeholder="email address"
+              onChange={handleEmail}
+              value={email}
             />
           </div>
 
@@ -84,13 +74,15 @@ export function RegisterPage() {
 
             <input
               className="w-4/5 p-0 pl-30 text-sm outline-0 border-b border-primaryColor text-center"
-              placeholder="password" onChange={handlePwd} value={password}
+              placeholder="password"
+              onChange={handlePwd}
+              value={password}
             />
-
           </div>
 
           <div className="flex justify-center">
-            <button onClick={handleSubmit}
+            <button
+              onClick={handleSubmit}
               type="submit"
               className="block px-5 py-3 text-sm font-medium text-white bg-primaryColor rounded-3xl w-5/6 "
             >
@@ -104,10 +96,10 @@ export function RegisterPage() {
             </p>
             <hr className="border-primaryColor w-40 mx-auto p-20 my-auto" />
             <div className="flex mx-auto relative bottom-36">
-              <div className="p-2" >
+              <div className="p-2">
                 <Google />
               </div>
-              <div className="p-2" >
+              <div className="p-2">
                 <Github />
               </div>
             </div>
