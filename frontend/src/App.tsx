c5b58b1b18ba9c13f71db9e45c287d406ebd1fb8
Viktor Avelino <viktor.avelino@gmail.com>
2022-02-22 09:19:39 -0500

    refactor: change Authcontext from app.tsx to main.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,29 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes } from "react-router-dom";
 import { LoginPage } from "./pages/LoginPage";
 import { RegisterPage } from "./pages/RegisterPage";
 
 // Main Application
 import { MainLayoutContainer } from "./pages/MainLayoutContainer";
-import MainPage from "./pages/MainPage";
 import { ForgotPassword } from "./pages/ForgotPassword";
 import { UserProfilePage } from "./pages/UserProfilePage";
 import { LandingPage } from "./pages/LandingPage";
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <div className="app">
       <Routes>
+        {/* Landing Page */}
         <Route path="/" element={<LandingPage />} />
-        <Route path="/dashboard" element={<MainLayoutContainer />} />
 
+        {/* Authentication */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
+
+        {/* Restricted Access */}
+        <Route path="/dashboard" element={<MainLayoutContainer />} />
+
         <Route path="/user-profile" element={<UserProfilePage />} />
       </Routes>
     </div>
