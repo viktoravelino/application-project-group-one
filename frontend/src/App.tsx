36f83461daffd82cf040ebf4e3f2ebdd9dc335b3
Viktor Avelino <viktor.avelino@gmail.com>
2022-02-22 09:28:15 -0500

    chore: restricted routes implemented

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import { LoginPage } from "./pages/LoginPage";
 import { RegisterPage } from "./pages/RegisterPage";
 
@@ -7,7 +7,7 @@ import { MainLayoutContainer } from "./pages/MainLayoutContainer";
 import { ForgotPassword } from "./pages/ForgotPassword";
 import { UserProfilePage } from "./pages/UserProfilePage";
 import { LandingPage } from "./pages/LandingPage";
-import { AuthContextProvider } from "./context/AuthContext";
+import { AuthContextProvider, useAuth } from "./context/AuthContext";
 
 function App() {
   return (
@@ -22,12 +22,42 @@ function App() {
         <Route path="/forgot-password" element={<ForgotPassword />} />
 
         {/* Restricted Access */}
-        <Route path="/dashboard" element={<MainLayoutContainer />} />
+        <Route
+          path="/dashboard"
+          element={
+            <RequireAuth>
+              <MainLayoutContainer />
+            </RequireAuth>
+          }
+        />
 
-        <Route path="/user-profile" element={<UserProfilePage />} />
+        {/* Restricted Access */}
+        <Route
+          path="/user-profile"
+          element={
+            <RequireAuth>
+              <UserProfilePage />
+            </RequireAuth>
+          }
+        />
       </Routes>
     </div>
   );
 }
 
 export default App;
+
+function RequireAuth({ children }: { children: JSX.Element }) {
+  let { currentUser } = useAuth();
+  let location = useLocation();
+
+  if (!currentUser) {
+    // Redirect them to the /login page, but save the current location they were
+    // trying to go to when they were redirected. This allows us to send them
+    // along to that page after they login, which is a nicer user experience
+    // than dropping them off on the home page.
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
+}
