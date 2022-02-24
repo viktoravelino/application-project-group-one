d28b671858ceb5ee6a1a11b2297435941f51f941
ahm86ed <ahm86ed@gmail.com>
2022-02-24 13:01:14 -0500

    Forgot page, done

--- frontend/src/pages/Auth/ForgotPassword/index.tsx
+++ frontend/src/pages/Auth/ForgotPassword/index.tsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-// import { Button } from "../../../components/AuthFormComponents/Button";
-import LogoSvg from "../../../components/Icons/Logo";
+//import { Button } from "../../../components/AuthFormComponents/Button";
 import { sendPasswordResetEmail } from "firebase/auth";
 import { auth } from "../../../config/firebase";
+import { CenteredContainerAuthForm } from "../../../components/AuthFormComponents/CenteredContainerAuthForm";
+import { Input } from "../../../components/AuthFormComponents/Input";
+
+import { BiUser } from "react-icons/bi";
+import { AuthForm } from "../../../components/AuthFormComponents/AuthForm";
+
 
 export const ForgotPassword = () => {
   const [email, setEmail] = useState("");
@@ -23,39 +28,42 @@ export const ForgotPassword = () => {
   };
 
   return (
-    <div id="loginPage" className="bg-bgColor ">
-      <div
-        id="formContainer"
-        className="flex justify-center items-center h-5/6  w-screen"
-      >
-        <form
-          onSubmit={(e) => handleResetPassword(e)}
-          className="mx-auto mb-0 space-y-4 bg-white  rounded-form w-80 h-4/5"
-        >
-          <div className="flex justify-center p-5">
-            <LogoSvg />
-          </div>
-          <div className="flex justify-center p-5">
-            <label className="font-black font-title text-black-600 text-xl">
-              Forgot Password
-            </label>
-          </div>
-          <input
+    <CenteredContainerAuthForm>
+    <AuthForm onSubmit={(e) => handleResetPassword(e)}>
+
+        <AuthForm.Header title="Forgot Password" />
+
+        <AuthForm.Body >
+
+        <Input
+            Icon={BiUser}
+            type="text"
             placeholder="email address"
             value={email}
-            onChange={(e) => setEmail(e.currentTarget.value)}
+            onChange={(e) => {
+              setEmail(e.currentTarget.value);
+            }}
           />
-          <div className="flex justify-center">
-            <button type="submit">Reset Password</button>
-          </div>
+</AuthForm.Body>
+
+        <AuthForm.Buttons>
+
+        <button
+            type="submit"
+            className="px-5 py-3 text-sm font-medium text-white bg-primaryColor rounded-3xl w-5/6 disabled:opacity-75"
+          >
+            Reset Password
+          </button>
+
           <span className="text-sm ">
             Remember you password?{" "}
             <Link to="/login" className="underline text-link">
               Login
             </Link>
           </span>
-        </form>
-      </div>
-    </div>
+          </AuthForm.Buttons>
+
+    </AuthForm>
+    </CenteredContainerAuthForm>
   );
 };
