5f5dc0753a7091b181e3b787930fe1bf0191b78c
Bechera <66968588+Pepsiman715@users.noreply.github.com>
2022-03-30 10:56:40 -0400

    remove unused variables and code

--- frontend/src/pages/BudgetsPage/ExportBudget/ExportBudget.tsx
+++ frontend/src/pages/BudgetsPage/ExportBudget/ExportBudget.tsx
@@ -1,28 +1,14 @@
 import {
-    addDoc,
-    deleteDoc,
-    doc,
-    getDoc,
-    getDocs,
     onSnapshot,
     query,
-    updateDoc,
     where,
   } from "firebase/firestore";
 
 import {useState} from 'react'
-import {json2csv, json2csvAsync } from 'json-2-csv';
-import { budgetsCollection, expensesCollection } from "../../../config/firebase";
+import { json2csvAsync } from 'json-2-csv';
+import { budgetsCollection } from "../../../config/firebase";
 import { Button } from "../../../components/Button/index";
 import { useAuth } from "../../../context/AuthContext";
-
-interface Budgets {
-    goalAmount : string;
-    id: string;
-    title: string;
-    totalSpent: number;
-    users: any
-}
 
 
 export const ExportBudget = () => {
@@ -41,22 +27,13 @@ export const ExportBudget = () => {
             onSnapshot(q, (querySnapshot) => {
             const test: any = [];
             querySnapshot.forEach((doc) => {
-              /**
-               * column: {
-               *  
-               * }
-               * 
-               */
-              /* [ id, title, amount ]
-                  [ { BudgetId: 'id', title: 'budgetTitle' }, { field: 'key2' }, 'key3', ... ]
-              */
 
               test.push({ id: doc.id, ...doc.data() });
             });
             setBudgets(test);
             let array : any = []
             test.map((budget : any, i : number) => {
-              console.log(budget)
+             
               const obj =  {
                   id: budget.id,
                   title: budget.title,
