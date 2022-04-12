b4ead24f46d6eb8bf79b18d3745c6e841cc688de
Viktor Avelino <viktor.avelino@gmail.com>
2022-04-12 15:04:09 -0400

    Fix merging conflicts

--- frontend/src/pages/CategoriesPage/index.tsx
+++ frontend/src/pages/CategoriesPage/index.tsx
@@ -123,7 +123,7 @@ const CategoryCard = ({
       className="budget-card px-3 py-3 
       border-[1px] border-gray-600
     rounded-lg shadow-lg shadow-gray-800
-    text-white
+    text-white dark:text-gray-700 dark:bg-gray-300
     flex flex-row justify-between items-center
     
     "
